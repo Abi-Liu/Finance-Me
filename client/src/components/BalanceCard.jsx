fb8e87f055d00d5018c6589debc136d025d306d5
Abi Liu <abiliu018@gmail.com>
2023-03-31 18:21:09 -0700

    change

--- client/src/components/BalanceCard.jsx
+++ client/src/components/BalanceCard.jsx
@@ -24,7 +24,7 @@ const BalanceCard = ({
   setAccount,
   id,
 }) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
