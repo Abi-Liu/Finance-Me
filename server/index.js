79f2348801c98dc80cd430e03372820392d777f2
Abi Liu <abiliu018@gmail.com>
2023-04-05 13:54:27 -0700

    changed cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://finance-me.netlify.app/",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
